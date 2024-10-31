1f484ddd0ead5fd2a3142195605cd586367a2a6c
Luffyfri1 <3396313655@qq.com>
2024-10-31 13:52:13 +0800

    Made some Updates

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("4: 除法 (/)");
         System.out.println("5.kaifang()");
         System.out.println(("asdjklkljdakjdjskld"));
+        System.out.println("ajkdjksdjkahrhvqklheerakljhcklar");
         System.out.print("输入你的选择 (1/2/3/4): ");
         int choice = scanner.nextInt();
 
